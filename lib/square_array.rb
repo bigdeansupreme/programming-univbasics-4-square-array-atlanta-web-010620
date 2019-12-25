faad6aebee4d6d41cb467e213a31465b7c1d7791
Dino Kunovac <dinokunovac@gmail.com>
2019-12-25 16:39:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   array = [2,5,9]
   counter = 0
   while counter < array.length do
-    puts square_array(array)**
+    puts square_array(counter)**
     counter += 1
   end
 end
